5dbbb3f29aeaa46e88e8eac0125dc0216c66554b
minh-biocommons <minh@biocommons.org.au>
2025-03-05 20:35:50 +1100

    navbar

---
+++ environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    auth0: {
+      domain: 'dev-bc.au.auth0.com',
+      clientId: 'JRpeNw5QAvtO85smXAMMvHXMKSxJpLnQ',
+      redirectUri: window.location.origin,
+    }
+  };
+  
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,17 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAuth0 } from '@auth0/auth0-angular';
 import { routes } from './app.routes';
+import { environment } from '../../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAuth0({
-      domain: 'dev-bc.au.auth0.com',
-      clientId: 'JRpeNw5QAvtO85smXAMMvHXMKSxJpLnQ',
+      domain: environment.auth0.domain,
+      clientId: environment.auth0.clientId,
       authorizationParams: {
-        redirect_uri: window.location.origin,
+        redirect_uri: environment.auth0.redirectUri,
       },
+      cacheLocation: 'localstorage',
     }),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
