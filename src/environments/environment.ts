623f5c092dc70bc6ecfa72fd141bb4a2464c32ec
minh-biocommons <minh@biocommons.org.au>
2025-03-07 00:16:26 +1100

    integrate auth0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   auth0: {
     domain: 'login.test.biocommons.org.au',
-    clientId: 'FxrMFnWjoQlv4WDxkxriueoTVeDS96Kg',
+    clientId: 'oXjPRRuMIVrzpJY7Ku2ojs0gZNmPVKmS',
     redirectUri: window.location.origin,
   },
 };
