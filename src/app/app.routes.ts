dcf588e47871efd72ceced4911c349f831a2901c
Minh Vu <minh@biocommons.org.au>
2025-03-13 13:39:26 +1100

    add NotFoundComponent and update routing; enhance user experience with 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { ServicesComponent } from './pages/user/services/services.component';
 import { AccessComponent } from './pages/user/access/access.component';
 import { PendingComponent } from './pages/user/pending/pending.component';
 import { RequestServiceComponent } from './pages/user/services/request-service/request-service.component';
+import { NotFoundComponent } from './pages/shared/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/services', pathMatch: 'full' },
@@ -18,4 +19,5 @@ export const routes: Routes = [
   },
   { path: 'access', component: AccessComponent },
   { path: 'pending', component: PendingComponent },
+  { path: '**', component: NotFoundComponent },
 ];
