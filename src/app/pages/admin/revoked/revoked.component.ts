088eb34b11fbcb7829b980db2979a8e57ea2f63a
Minh Vu <minh@biocommons.org.au>
2025-03-25 16:28:43 +1100

    add revoked user selection and loading spinner; enhance revoked users management and user experience

--- src/app/pages/admin/revoked/revoked.component.ts
+++ src/app/pages/admin/revoked/revoked.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { LoadingSpinnerComponent } from '../../../shared/components/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-revoked',
-  imports: [],
+  imports: [LoadingSpinnerComponent],
   templateUrl: './revoked.component.html',
-  styleUrl: './revoked.component.css'
+  styleUrl: './revoked.component.css',
 })
 export class RevokedComponent {
+  revokedUserSelected = false;
+  loading = false;
+  selectedRevokedUserName = '';
 
+  selectRevokedUser(revokedUserName: string): void {
+    this.revokedUserSelected = true;
+    this.selectedRevokedUserName = revokedUserName;
+  }
 }
